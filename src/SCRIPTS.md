e84412a67dfc3e9331f2fcb35edbda4430e175cf
90zlaya <contact@zlatanstajic.com>
2019-11-13 13:08:15 +0100

    PHP Switch script added

--- src/SCRIPTS.md
+++ src/SCRIPTS.md
@@ -1,3 +1,4 @@
 # List of available scripts
 
+* php-switch.sh: Switch main version of PHP on OS
 * ssh-connect.sh: Connect to the server via SSH connection
---
+++ src/php-switch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# List versions of PHP installed on your OS
+PHP_VERSIONS_INSTALLED=(
+  5.6
+  7.1
+  7.2
+  7.3
+);
+
+# Parameter #1 represents PHP version to be set
+PHP_VERSION=$1;
+
+for version in ${PHP_VERSIONS_INSTALLED[*]}
+do
+  sudo a2dismod php${version};
+done
+
+sudo update-alternatives --set php /usr/bin/php${PHP_VERSION};
+sudo a2enmod php${PHP_VERSION};
+sudo service apache2 restart;
+php --version;
