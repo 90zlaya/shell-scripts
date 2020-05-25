7d5936dd241b291dde867c216be84082b324decd
90zlaya <contact@zlatanstajic.com>
2020-05-25 15:41:24 +0200

    Packages added to apm script

--- src/apm-extensions.sh
+++ src/apm-extensions.sh
@@ -2,19 +2,24 @@
 
 # List of apm extensions
 APM_EXTENSIONS=(
+  "atom-beautify"
   "atom-clock"
   "atom-vue"
   "busy-signal"
   "csstree-validator"
   "emmet"
   "file-icons"
+  "highlight-selected"
   "intentions"
   "linter"
   "linter-jsonlint"
   "linter-php"
   "linter-ui-default"
+  "minimap"
+  "pigments"
   "platformio-ide-terminal"
   "remote-ftp"
+  "todo-show"
 );
 
 for extension in ${APM_EXTENSIONS[*]}
