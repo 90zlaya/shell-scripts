6e1fd3386731990ce3708cc13f153f6eaa6de978
90zlaya <contact@zlatanstajic.com>
2020-11-11 00:14:56 +0100

    Removed ssh-connect.sh, updated php-switch

--- src/README.md
+++ src/README.md
@@ -4,7 +4,6 @@
 ## Table of Contents
 
 1. [PHP Switch](#php-switch)
-1. [SSH Connect](#ssh-connect)
 1. [Sync Forked](#sync-forked)
 1. [Git Pull](#git-pull)
 1. [SVN Update](#svn-update)
@@ -14,36 +13,22 @@
 1. ### PHP Switch
 
 * File: [php-switch.sh](./php-switch.sh)
-* Arguments: Required
+* Arguments: php-version
 * Description: Switch main version of PHP on OS
 
 You would have to update this script and list versions of PHP installed on your operating system inside `PHP_VERSIONS_INSTALLED` array in order to properly run it.
 
 ```bash
+# Show Help
+bash php-switch.sh -h
+
 # How to switch to PHP version 7.3
 bash php-switch.sh 7.3
 ```
 
 [⬆ back to top](#table-of-contents)
 
-2. ### SSH Connect
-
-* File: [ssh-connect.sh](./ssh-connect.sh)
-* Arguments: None
-* Description: Connect to the server via SSH connection
-
-You would have to update this script and set `USER`, `HOST` and `PORT` before running it.
-
-```bash
-# Connect to the server via SSH
-bash ssh-connect.sh
-```
-
-You will be asked for password before server lets you to in.
-
-[⬆ back to top](#table-of-contents)
-
-3. ### Sync Forked
+2. ### Sync Forked
 
 * File: [sync-forked.sh](./sync-forked.sh)
 * Arguments: 1st is optional, 2nd is optional
@@ -59,7 +44,7 @@ bash <path-to-the-shell-script>sync-forked.sh
 
 [⬆ back to top](#table-of-contents)
 
-4. ### Git Pull
+3. ### Git Pull
 
 * File: [git-pull.sh](./git-pull.sh)
 * Arguments: None
@@ -72,7 +57,7 @@ bash <path-to-the-shell-script>git-pull.sh
 
 [⬆ back to top](#table-of-contents)
 
-5. ### SVN Update
+4. ### SVN Update
 
 * File: [svn-update.sh](./svn-update.sh)
 * Arguments: None
--- src/php-switch.sh
+++ src/php-switch.sh
@@ -3,39 +3,135 @@
 ################################################################################
 # Script name : php-switch.sh
 # Description : Switch main version of PHP on OS
-# Arguments   : PHP_VERSION
+# Arguments   : php-version
 # Author      : 90zlaya
 # Email       : contact@zlatanstajic.com
 # Licence     : MIT
 ################################################################################
 
+SCRIPT_NAME="`basename $(readlink -f $0)`"
+SCRIPT_DIR="`dirname $(readlink -f $0)`"
+
+################################################################################
+
+# PHP versions installed on your OS (remove # do declare as installed)
+PHP_VERSIONS_INSTALLED=(
+  #"5.6"
+  #"7.0"
+  #"7.1"
+  #"7.2"
+  #"7.3"
+  #"7.4"
+)
+
 # Parameter #1 represents PHP version to be set
 PHP_VERSION=$1
 
 ################################################################################
+# Show help
+################################################################################
 
-# List versions of PHP installed on your OS
-PHP_VERSIONS_INSTALLED=(
-  "5.6"
-  "7.0"
-  "7.1"
-  "7.2"
-  "7.3"
-  "7.4"
-)
+Help()
+{
+  echo ""
+  echo -e "\e[1mRunning $SCRIPT_NAME\e[0m"
+  echo "Description: Switch main version of PHP on OS"
+  
+  if [ ${#PHP_VERSIONS_INSTALLED[@]} -eq 0 ]
+  then
+    echo "Update PHP_VERSIONS_INSTALLED in $SCRIPT_DIR/$SCRIPT_NAME"
+  else
+    INSTALLED=""
+    for version in ${PHP_VERSIONS_INSTALLED[*]}
+    do
+      INSTALLED+="${version} "
+    done
+    echo "Installed versions: $INSTALLED"
+  fi
+
+  echo ""
+  echo "Show this help : $SCRIPT_NAME -h"
+  echo "Switch version : $SCRIPT_NAME <php-version>"
+  echo ""
+}
 
 ################################################################################
+# Getting parameters
+################################################################################
+
+GetParameters()
+{
+  if [ $# -eq 1 ]
+  then
+    if [ "x$1" = "x-h" ]
+    then
+      Help
+      End 0
+    fi
+    # Checking PHP version
+    if [[ ! " ${PHP_VERSIONS_INSTALLED[@]} " =~ " ${PHP_VERSION} " ]]
+    then
+      Help
+      End 1 "Incorrect parameters: Non existing PHP version"
+    fi
+  else
+    Help
+    End 1 "Incorrect parameters"
+  fi
+}
+
+################################################################################
+# Shell terminates
+################################################################################
+
+End()
+{
+  if [ $1 -eq 0 ]
+  then
+    echo "Script $SCRIPT_NAME finishing OK"
+    echo ""
+    exit 0
+  else
+    echo -e "Script $SCRIPT_NAME finishing with \e[1mERROR [$2]\e[0m"
+    echo ""
+    exit 1
+  fi
+}
+
+################################################################################
+# Testing unique run
+################################################################################
+
+EndIfRunning()
+{
+  STARTED=`ps -f -u$USER | grep "$SCRIPT_NAME" | grep -v $$ | grep -v vi | grep -v log | grep -v grep | grep -v gedit`
+  
+  if [[ "x$STARTED" != "x" ]]
+  then
+    End 1 "$SCRIPT_NAME already running."
+  fi
+}
+
+################################################################################
+# Executing all
+################################################################################
+
+echo ""
+echo "Script $SCRIPT_NAME starting..."
+
+GetParameters $@
+EndIfRunning
 
 for version in ${PHP_VERSIONS_INSTALLED[*]}
 do
   sudo a2dismod php${version}
 done
 
-################################################################################
-
 sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
 sudo a2enmod php${PHP_VERSION}
 sudo service apache2 restart
 php --version
+
+End 0
 
 ################################################################################
